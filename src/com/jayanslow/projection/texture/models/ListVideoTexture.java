fcc1d9adfd3e8b1c57f425c6a3b3aa8b50473c2b
Jay Anslow <jay@anslow.me.uk>
2014-04-30 04:16:28 +0100

    Added remove and add methods to ListVideoTexture

--- src/com/jayanslow/projection/texture/models/ListVideoTexture.java
+++ src/com/jayanslow/projection/texture/models/ListVideoTexture.java
@@ -2,10 +2,32 @@ package com.jayanslow.projection.texture.models;
 
 import java.util.List;
 
+import com.jayanslow.projection.texture.listeners.TextureListener;
+
 public class ListVideoTexture extends AbstractVideoTexture {
+
+	private final TextureListener	internalListener	= new TextureListener() {
+
+															@Override
+															public void textureChange(Texture texture) {
+																fireTextureChange();
+															}
+
+															@Override
+															public void textureFrameChange(int current, int old) {}
+
+														};
 
 	public ListVideoTexture(List<ImageTexture> images) {
 		super(TextureType.LIST, images);
+		for (ImageTexture t : images)
+			t.addTextureListener(internalListener);
+	}
+
+	public void add(ImageTexture t, int index) {
+		images.add(index, t);
+		t.addTextureListener(internalListener);
+		fireTextureChange();
 	}
 
 	@Override
@@ -13,7 +35,10 @@ public class ListVideoTexture extends AbstractVideoTexture {
 		return images;
 	}
 
-	public void markDirty() {
+	public void remove(int index) {
+		Texture t = images.remove(index);
+		if (t != null)
+			t.removeTextureListener(internalListener);
 		fireTextureChange();
 	}
 
